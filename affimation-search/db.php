5dd708c135a1e5aa1fb216392e45a06436d6666d
Karo Ej <karo_ej@noiseofeedomair.home>
2025-01-26 20:24:23 +0100

    first steps to affirmation-search

---
+++ affimation-search/db.php
@@ -0,0 +1,15 @@
+<?php 
+require __DIR__.'/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+
+?>
